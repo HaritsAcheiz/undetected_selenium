6a640a70aa154eafac60d835d4822ba74091c35d
HARITS <harits.muhammad.only@gmail.com>
2022-11-28 21:41:02 +0700

    build webdriver setup not completed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import json
+import random
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+from fake_useragent import UserAgent
 
 
 class UndetectedSelenium:
@@ -15,30 +19,49 @@ class UndetectedSelenium:
         soup = bs(response.content, 'html.parser').find_all("td", {"class": "blob-code blob-code-inner js-file-line"})
         for i in soup:
             proxies.append(i.text)
+
         return proxies
 
     def working_proxy(self, proxies):
         url = 'https://www.google.com'
         proxy = []
-        for i in proxies:
-            formated_proxy = {"http":i}
-            print(f'checking {formated_proxy}')
-            try:
-                response = requests.get(url=url, proxies=formated_proxy, timeout=3)
-                print(response.content)
-                proxy.append(i)
-                print(f'{i} selected')
+        for i, item in enumerate(proxies):
+            if i < 10:
+                formated_proxy = {"http":item}
+                print(f'checking {formated_proxy}')
+                try:
+                    with requests.Session() as session:
+                        session.get(url=url, proxies=formated_proxy, timeout=3)
+                    proxy.append(item)
+                    print(f'{item} selected')
+                except Exception as e:
+                    print("not working")
+                    pass
+            else:
                 break
-            except Exception as e:
-                print("not working")
-                pass
+
         return proxy
+
+    def webdriver_setup(self, proxy):
+        host, port = proxy.split(sep=':')
+        ua = UserAgent()
+        useragent = ua.firefox
+        firefox_options = FirefoxOptions()
+        firefox_options.set_preference("network.proxy.type", 1)  # 1 for MANUAL
+        firefox_options.set_preference("network.proxy.http", host)
+        firefox_options.set_preference("network.proxy.http_port", int(port))
+        firefox_options.set_preference("network.proxy.ssl", host)
+        firefox_options.set_preference("network.proxy.ssl_port", int(port))
+        # firefox_options.headless = True
+        firefox_options.set_preference("")
+
+        return firefox_options
 
 if __name__ == '__main__':
     us = UndetectedSelenium()
     proxy_lists = us.get_proxies()
     selected_proxy = us.working_proxy(proxies=proxy_lists)
-    print(selected_proxy)
+
 
 
 
