6f9f187c3f2e67a9bf34bcd6daba2b45f52a9405
Haritz <harits.muhammad.only@gmail.com>
2022-12-05 13:53:02 +0700

    success to rotate IP

--- selenium_wm.py
+++ selenium_wm.py
@@ -1,8 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.core.http import HttpClient
+from webdriver_manager.core.download_manager import WDMDownloadManager
+import requests
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+class CustomHttpClient(HttpClient):
+    def get(self, url, params=None):
+        proxies={
+            'http': 'socks5:// *proxy adress*',
+            'https': 'socks5:// *proxy adress*'
+         }
+        return requests.get(url, params, proxies=proxies, verify=False)
+
+http_client = CustomHttpClient()
+download_manager = WDMDownloadManager(http_client)
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager(download_manager=download_manager).install()))
 driver.get('https://www.google.com')
 print(driver.title)
 driver.quit()
+
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager(download_manager=download_manager).install()),options=opt)`
--- try_ip_rotate.py
+++ try_ip_rotate.py
@@ -1,4 +1,3 @@
-import time
 from random import choice
 from fake_useragent import UserAgent
 from selenium import webdriver
@@ -6,53 +5,51 @@ from selenium.webdriver import Proxy
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.proxy import ProxyType
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-
-# s = Service(r'C:/geckodriver-v0.32.0-win32/geckodriver.exe')
-# path = r"C:/geckodriver-v0.32.0-win32/geckodriver.exe"
 
 def rotate():
-    proxy_string = "47.241.191.76:40059 47.241.191.76:40060 8.214.112.193:40034"
-    proxy_list = proxy_string.split(sep=" ")
+    proxy_string = "147.135.65.90:10030	147.135.65.90:10032	51.81.109.223:10010"
+    proxy_list = proxy_string.split(sep="\t")
     proxy = choice(proxy_list)
-    # proxy = "147.135.65.90:40034"
+    ip, port = proxy.split(sep=':')
     ua = UserAgent()
     useragent = ua.firefox
     firefox_options = Options()
-    firefox_options.binary = r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe'
-    firefox_options.proxy = Proxy(
-        {
-            'proxyType': ProxyType.MANUAL,
-            "socksVersion": 5,
-            'httpProxy': proxy,
-            'sslProxy': proxy,
-            "socksProxy": proxy,
-            'noProxy': ''
-        }
-    )
-    # firefox_options.headless = True
+
+    firefox_options.headless = True
     firefox_options.add_argument('--no-sandbox')
 
     firefox_options.set_preference("general.useragent.override", useragent)
+    firefox_options.set_preference('network.proxy.type', 1)
+    firefox_options.set_preference('network.proxy.socks', ip)
+    firefox_options.set_preference('network.proxy.socks_port', int(port))
+    firefox_options.set_preference('network.proxy.socks_remote_dns', True)
 
     firefox_options.set_capability("acceptSslCerts", True)
     firefox_options.set_capability("acceptInsecureCerts", True)
     firefox_options.set_capability("ignore-certificate-errors", True)
-    # driver = webdriver.Firefox(service=s, options=firefox_options)
+
     driver = webdriver.Firefox(options=firefox_options)
     return driver
 
-    # return webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
+def reset_ip():
+    firefox_options = Options()
+    # firefox_options.binary = r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe'
+    firefox_options.proxy = Proxy(
+        {
+            'proxyType': ProxyType.SYSTEM
+        }
+    )
+    driver = webdriver.Firefox(options=firefox_options)
+    return driver
 
 if __name__ == '__main__':
     counter = 1
-    driver = rotate()
     while counter < 10:
-        if counter % 2 == 0:
-            driver = rotate()
-        else:
-            pass
+        # if counter % 2 == 0:
+        #     driver = rotate()
+        # else:
+        #     pass
+        driver = rotate()
         driver.delete_all_cookies()
         driver.fullscreen_window()
         driver.implicitly_wait(10)
