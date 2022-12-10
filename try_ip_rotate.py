e7ed32517b1c859a55211b1e9c4955b66e7ebb61
HARITS <harits.muhammad.only@gmail.com>
2022-12-11 01:45:32 +0700

    free proxy new is working, face cookie policy

--- try_ip_rotate.py
+++ try_ip_rotate.py
@@ -1,22 +1,19 @@
+import time
 from random import choice
-from time import sleep
-
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from selenium import webdriver
+from selenium.common import NoSuchElementException, WebDriverException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import re
 import os
 import csv
-
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
-# proxy_string = "67.207.190.112:20000:Narusegawa:M1tsut4n1,67.207.190.114:20000:Narusegawa:M1tsut4n1,67.207.190.116:20000:Narusegawa:M1tsut4n1"
-# proxy_list = proxy_string.split(sep=",")
 
 def webdriver_setup(proxies = None):
     ip, port = proxies.split(sep=':')
@@ -63,26 +60,26 @@ def to_csv(datas=None, filepath='C:/project/etsy/result/result_sel.csv'):
 
 def get_proxy():
     with requests.Session() as s:
-        response = s.get('https://www.us-proxy.org/')
+        response = s.get('https://free-proxy-list.net/')
     s.close()
     soup = BeautifulSoup(response.text, 'html.parser')
     list_data = soup.select('table.table.table-striped.table-bordered>tbody>tr')
     proxy_data= []
+    blocked_cc = ['IR','RU']
     for i in list_data:
         ip = i.select_one('tr > td:nth-child(1)').text
         port = i.select_one('tr > td:nth-child(2)').text
-        proxy_data.append(f'{ip}:{port}')
-        # nation = i.select_one('tr > td:nth-child(3)').text
-        # if nation != 'RU':
-        #     proxy_data.append(f'{ip}:{port}')
-        # else:
-        #     continue
+        cc = i.select_one('tr > td:nth-child(3)').text
+        if cc in blocked_cc:
+            continue
+        else:
+            proxy_data.append(f'{ip}:{port}')
     return proxy_data
 
 def choose_proxy(proxies):
     proxy=[]
     for i, item in enumerate(proxies):
-        if i < len(proxies) and len(proxy) < 3:
+        if i < len(proxies) and len(proxy) < 5:
             formated_proxy = {
                 "http": f"http://{item}",
                 "https": f"http://{item}"
@@ -101,45 +98,51 @@ def choose_proxy(proxies):
             break
     return proxy
 
-if __name__ == '__main__':
-    page = 249
-    # proxy_list = choose_proxy(get_proxy())
-    # print(proxy_list)
-    proxy_list = ['35.193.113.186:80', '216.176.187.99:8889', '75.126.253.8:8080']
-    proxy = choice(proxy_list)
-    # ip, port, user, pw = proxy.split(sep=':')
-    ip, port = proxy.split(sep=':')
-    url = 'https://www.etsy.com/'
-    search_term = 'necklace'
-    driver = webdriver_setup(proxies=proxy)
-    driver.delete_all_cookies()
-    driver.fullscreen_window()
-    driver.implicitly_wait(10)
-    driver.get(url)
-    sleep(10)
-    driver.find_element(By.ID, 'global-enhancements-search-query').send_keys(search_term + Keys.RETURN)
-    sleep(10)
-    query_url = driver.current_url
-    search_url = f'{query_url}&ref=pagination&page={str(page)}'
-    driver.quit()
+def get_data(page=1, proxy=None):
+    while True:
+        try:
+            driver = webdriver_setup(proxies=proxy)
+            driver.delete_all_cookies()
+            driver.fullscreen_window()
+            # driver.implicitly_wait(10)
+            driver.get(url)
+            WebDriverWait(driver,10).until(ec.presence_of_element_located((By.ID, 'global-enhancements-search-query')))
+            driver.find_element(By.ID, 'global-enhancements-search-query').send_keys(search_term + Keys.RETURN)
+        # WebDriverWait(driver,10).until(ec.presence_of_element_located((By.ID, 'content')))
+            time.sleep(10)
+            query_url = driver.current_url
+            driver.quit()
+            break
+        except WebDriverException as e:
+            print(e)
+            proxy = choice(proxy_list)
+            driver.quit()
+            continue
     data = {'url': '', 'title': '', 'price': '', 'sales': ''}
     res = []
-    #
     while True:
-        if page % 3 == 0:
+        if page % 3 == 0 :
             proxy = choice(proxy_list)
         else:
             pass
-        search_url = f'{query_url}&ref=pagination&page={str(page)}'
-        driver = webdriver_setup(proxy)
-        driver.delete_all_cookies()
-        driver.fullscreen_window()
-        driver.implicitly_wait(10)
+        search_url = f"{query_url}&ref=pagination&page={str(page)}"
+        while True:
+            try:
+                driver = webdriver_setup(proxy)
+                driver.delete_all_cookies()
+                driver.fullscreen_window()
+                # driver.implicitly_wait(10)
+                driver.get(search_url)
+                break
+            except WebDriverException as e:
+                print(e)
+                proxy = choice(proxy_list)
+                continue
 
-        driver.get(search_url)
+        WebDriverWait(driver,10).until(ec.presence_of_element_located((By.ID, 'content')))
         try:
             result = driver.find_elements(By.CSS_SELECTOR,
-                                           'ol.wt-grid.wt-grid--block.wt-pl-xs-0.tab-reorder-container > li')
+                                          'ol.wt-grid.wt-grid--block.wt-pl-xs-0.tab-reorder-container > li')
         except Exception as e:
             print(e)
             break
@@ -148,8 +151,8 @@ if __name__ == '__main__':
             try:
                 data['url'] = i.find_element(By.CSS_SELECTOR, 'a.listing-link.wt-display-inline-block').get_attribute(
                     'href')
-            except TypeError:
-                print('not found')
+            except (TypeError, NoSuchElementException) as e:
+                print(e)
                 break
             data['title'] = i.find_element(By.CSS_SELECTOR, 'h3.wt-text-caption.v2-listing-card__title').text
             data['title'] = re.sub(r'\n +', '', data['title'])
@@ -160,15 +163,32 @@ if __name__ == '__main__':
                 data['sales'] = i.find_element(By.CSS_SELECTOR,
                                                'span.wt-text-caption.wt-text-gray.wt-display-inline-block.wt-nudge-l-3.wt-pr-xs-1').text
                 data['sales'] = re.sub(r'\D', '', data['sales'])
-            except AttributeError:
+            except (AttributeError, NoSuchElementException) as e:
                 data['sales'] = 0
             res.append(data.copy())
 
         driver.quit()
         print(f'\n{len(res)} products collected by selenium from page {page}')
         page += 1
+    return res
 
+
+if __name__ == '__main__':
+    # Define variable input
+    page = 249
+    url = 'https://www.etsy.com'
+    search_term = 'necklace'
+
+    # Get proxy list
+    proxy_list = choose_proxy(get_proxy())
+    print(proxy_list)
+
+    # random choice proxy from proxy list
+    # proxy_list = ['134.238.252.143:8080', '185.143.146.171:8080', '213.230.97.98:3128']
+    proxy = choice(proxy_list)
+
+    # Get data
+    res = get_data(page=page, proxy=proxy)
+
+    # Export to csv
     to_csv(datas=res)
-    # my_proxies = get_proxy()
-    # working_proxies = choose_proxy(my_proxies)
-    # print(working_proxies)
